56cf0e526349bc120e85f4923436735bb7b43eaf
Poker <498163198@qq.com>
2019-11-08 19:36:12 +0800

    story

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        try {
+            shortstorywrite.read(1441);
+        } catch (com.company.story story) {
+            story.printStackTrace();
+        }
+    }
+}
